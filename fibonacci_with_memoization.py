5f0e428896ac006c0798a96458cbd4bb923cdc3a
Thousif Ameer khan <thousifameerkhan@gmail.com>
2019-05-14 23:52:01 +0530

    Add files via upload

---
+++ fibonacci_with_memoization.py
@@ -0,0 +1,28 @@
+import sys,time
+
+def fib(n):
+    if (n==2) or (n==1):
+        return 1
+    else:
+        return fib(n-1)+fib(n-2) 
+
+def fib_mem(n):
+    if (arr[n]==-1):
+        p=fib_mem(n-1)+fib_mem(n-2)
+        arr[n]=p
+        return p
+    else:
+        return arr[n]
+
+
+arr = [-1] * 50
+arr[1]=1
+arr[2]=1
+startime=time.time()
+b=int(sys.argv[1])
+a=fib_mem(b)
+elapsedtime=time.time()-startime
+print (a)
+print ("Time Executed is : "+str(elapsedtime) )
+""" for e in arr:
+    print("The Element  in array are "+str(e)) """
