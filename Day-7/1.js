369be47a20723be4c6a53258501ea618e7567145
Thousif Ameer khan <thousifameerkhan@gmail.com>
2020-07-06 07:47:07 +0530

    Add files via upload

---
+++ Day-7/1.js
@@ -0,0 +1,12 @@
+function f2()
+{
+document.getElementById('TestButton').innerHTML='This is to Test External Script';
+//window.alert('This is a window alert');
+console.log('This is from console logger');
+
+}
+
+function docu_write()
+{
+    document.write("This is to Test document.write");
+}
